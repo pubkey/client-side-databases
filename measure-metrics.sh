c4559616c9cdc6f0a51a92857ad34fa2144d46e8
pubkey <8926560+pubkey@users.noreply.github.com>
2022-01-31 01:38:34 +0100

    UPDATE stats

--- measure-metrics.sh
+++ measure-metrics.sh
@@ -9,6 +9,7 @@ npm run server:firebase:setup && npm run test:firebase
 npm run test:pouchdb
 npm run test:rxdb-pouchdb
 npm run test:rxdb-lokijs
+npm run test:rxdb-dexie
 npm run test:watermelondb
 
 npm run aggregate-metrics
