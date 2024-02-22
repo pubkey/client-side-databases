d777b28b710908352585bc0406ecfca40ed95100
Matt Riese <riese.matt@gmail.com>
2024-02-21 19:14:32 -0800

    Add second message type conversion utility

--- projects/electric-sql/src/app/util/convertMessageType.ts
+++ projects/electric-sql/src/app/util/convertMessageType.ts
@@ -1,7 +1,14 @@
 import { Message } from 'src/shared/types';
 import { Messages } from '../generated/client';
 
-export const convertMessageType = (postgresMessage: Messages): Message => {
+export const convertMessageSqlToNosql = (
+  postgresMessage: Messages
+): Message => {
   const { created_at, ...rest } = postgresMessage;
   return { ...rest, createdAt: Number(created_at) };
+};
+
+export const convertMessageNosqlToSql = (message: Message): Messages => {
+  const { createdAt, ...rest } = message;
+  return { ...rest, created_at: BigInt(createdAt) };
 };
